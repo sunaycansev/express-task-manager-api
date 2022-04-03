aef697a0b48e1e370b6652c2578ffbd49b7ac611
sunaycansev <fsunaycansev@gmail.com>
2022-04-03 16:19:45 +0300

    fix:secure

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import connectDB from "./db/connect.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 import tasks from "./routes/tasks.js";
 
@@ -15,7 +17,7 @@ app.use("/api/v1/tasks", tasks);
 const port = 5000;
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`Server started on port ${port}`));
   } catch (e) {
     console.log(e);
--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-const connecionString =
-  "mongodb+srv://hexe137:18032002@cluster0.dfssm.mongodb.net/TaskManager?retryWrites=true&w=majority";
-
 export default async function connectDB(url) {
-  return await mongoose.connect(connecionString);
+  return await mongoose.connect(url);
 }
